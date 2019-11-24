4989c9a557940892c70a171858ee8954a754fa84
OKAMURA, Yasunobu <okamura@informationsea.info>
2019-11-24 12:52:54 +0900

    implementing charts

--- src/chart.rs
+++ src/chart.rs
@@ -152,6 +152,42 @@ impl Default for ChartLine {
     }
 }
 
+/// Struct to represent a chart fill.
+#[derive(Clone, PartialEq, PartialOrd)]
+pub struct ChartFill {
+    /// The chart font color.
+    pub color: FormatColor,
+    /// Turn off/hide line. Set to false or true.
+    pub none: bool,
+    /// Set the transparency of the fill. 0 - 100. Default 0.
+    pub transparency: u8,
+}
+
+impl ChartFill {
+    pub fn new() -> Self {
+        ChartFill::default()
+    }
+
+    fn value(&self) -> libxlsxwriter_sys::lxw_chart_fill {
+        libxlsxwriter_sys::lxw_chart_fill {
+            color: self.color.value(),
+            none: convert_bool(self.none),
+            transparency: self.transparency,
+            has_color: convert_bool(false),
+        }
+    }
+}
+
+impl Default for ChartFill {
+    fn default() -> Self {
+        ChartFill {
+            color: FormatColor::Black,
+            none: false,
+            transparency: 0,
+        }
+    }
+}
+
 /// Struct to represent an Excel chart data series.
 /// This struct is created using the chart.add_series() function. It is used in functions that modify a chart series but the members of the struct aren't modified directly.
 pub struct ChartSeries<'a> {
@@ -297,6 +333,32 @@ impl<'a> ChartSeries<'a> {
         self._workbook.const_str.borrow_mut().push(name_vec);
     }
 
+    /// The `ChartSeries.set_name_range()` function can be used to set a series name range and is an alternative to using `ChartSeries.set_name()` and a string formula:
+    /// ```rust
+    /// # use xlsxwriter::*;
+    /// # fn main() { let _ = run(); }
+    /// # fn run() -> Result<(), XlsxError> {
+    /// # let workbook = Workbook::new("test-chart_series-set_name_range-1.xlsx");
+    /// # let mut worksheet = workbook.add_worksheet(None)?;
+    /// # write_worksheet(&mut worksheet)?; // write worksheet contents
+    /// # let mut chart = workbook.add_chart(ChartType::Column);
+    /// let mut series = chart.add_series(None, Some("=Sheet1!$B$2:$B$6"));
+    /// series.set_name_range("Sheet1", 0, 1); // =Sheet1!$B$1
+    /// # worksheet.insert_chart(1, 3, &chart)?;
+    /// # workbook.close()
+    /// # }
+    /// # fn write_worksheet(worksheet: &mut Worksheet) -> Result<(), XlsxError> {
+    /// # worksheet.write_string(0, 0, "Set 1", None)?;
+    /// # worksheet.write_string(0, 1, "Set 2", None)?;
+    /// # worksheet.write_string(0, 2, "Set 3", None)?;
+    /// # for i in 1..6 {
+    /// #     worksheet.write_number(i, 0, (i*10).into(), None)?;
+    /// #     worksheet.write_number(i, 1, (i*10 + 2).into(), None)?;
+    /// #     worksheet.write_number(i, 2, (i*10 + 4).into(), None)?;
+    /// # }
+    /// # Ok(())
+    /// # }
+    /// ```
     pub fn set_name_range(&mut self, sheet_name: &str, row: WorksheetRow, column: WorksheetCol) {
         let sheet_name_vec = convert_str(sheet_name);
         unsafe {
@@ -310,9 +372,131 @@ impl<'a> ChartSeries<'a> {
         self._workbook.const_str.borrow_mut().push(sheet_name_vec);
     }
 
+    /// Set the line/border properties of a chart series:
+    /// ```rust
+    /// # use xlsxwriter::*;
+    /// # fn main() { let _ = run(); }
+    /// # fn run() -> Result<(), XlsxError> {
+    /// # let workbook = Workbook::new("test-chart_series-set_line-1.xlsx");
+    /// # let mut worksheet = workbook.add_worksheet(None)?;
+    /// # write_worksheet(&mut worksheet)?; // write worksheet contents
+    /// # let mut chart = workbook.add_chart(ChartType::Column);
+    /// let mut series1 = chart.add_series(None, Some("=Sheet1!$A$2:$A$6"));
+    /// let mut series2 = chart.add_series(None, Some("=Sheet1!$B$2:$B$6"));
+    /// let mut series3 = chart.add_series(None, Some("=Sheet1!$C$2:$C$6"));
+    /// let mut chart_line = ChartLine::new();
+    /// chart_line.color = FormatColor::Red;
+    /// series1.set_line(&chart_line);
+    /// series2.set_line(&chart_line);
+    /// series3.set_line(&chart_line);
+    /// # worksheet.insert_chart(1, 3, &chart)?;
+    /// # workbook.close()
+    /// # }
+    /// # fn write_worksheet(worksheet: &mut Worksheet) -> Result<(), XlsxError> {
+    /// # worksheet.write_string(0, 0, "Set 1", None)?;
+    /// # worksheet.write_string(0, 1, "Set 2", None)?;
+    /// # worksheet.write_string(0, 2, "Set 3", None)?;
+    /// # for i in 1..6 {
+    /// #     worksheet.write_number(i, 0, (i*10).into(), None)?;
+    /// #     worksheet.write_number(i, 1, (i*10 + 2).into(), None)?;
+    /// #     worksheet.write_number(i, 2, (i*10 + 4).into(), None)?;
+    /// # }
+    /// # Ok(())
+    /// # }
+    /// ```
+    /// ![Result Image](https://github.com/informationsea/xlsxwriter-rs/raw/master/images/test-chart_series-set_line-1.png)
     pub fn set_line(&mut self, line: &ChartLine) {
         unsafe {
             libxlsxwriter_sys::chart_series_set_line(self.chart_series, &mut line.value());
+        }
+    }
+
+    /// Set the fill properties of a chart series:
+    /// ```rust
+    /// # use xlsxwriter::*;
+    /// # fn main() { let _ = run(); }
+    /// # fn run() -> Result<(), XlsxError> {
+    /// # let workbook = Workbook::new("test-chart_series-set_fill-1.xlsx");
+    /// # let mut worksheet = workbook.add_worksheet(None)?;
+    /// # write_worksheet(&mut worksheet)?; // write worksheet contents
+    /// # let mut chart = workbook.add_chart(ChartType::Column);
+    /// # let mut series1 = chart.add_series(None, Some("=Sheet1!$A$2:$A$6"));
+    /// # let mut series2 = chart.add_series(None, Some("=Sheet1!$B$2:$B$6"));
+    /// # let mut series3 = chart.add_series(None, Some("=Sheet1!$C$2:$C$6"));
+    /// let mut chart_fill_1 = ChartFill::new();
+    /// chart_fill_1.color = FormatColor::Red;
+    /// let mut chart_fill_2 = ChartFill::new();
+    /// chart_fill_2.color = FormatColor::Yellow;
+    /// let mut chart_fill_3 = ChartFill::new();
+    /// chart_fill_3.color = FormatColor::Green;
+    /// series1.set_fill(&chart_fill_1);
+    /// series2.set_fill(&chart_fill_2);
+    /// series3.set_fill(&chart_fill_3);
+    /// # worksheet.insert_chart(1, 3, &chart)?;
+    /// # workbook.close()
+    /// # }
+    /// # fn write_worksheet(worksheet: &mut Worksheet) -> Result<(), XlsxError> {
+    /// # worksheet.write_string(0, 0, "Set 1", None)?;
+    /// # worksheet.write_string(0, 1, "Set 2", None)?;
+    /// # worksheet.write_string(0, 2, "Set 3", None)?;
+    /// # for i in 1..6 {
+    /// #     worksheet.write_number(i, 0, (i*10).into(), None)?;
+    /// #     worksheet.write_number(i, 1, (i*10 + 2).into(), None)?;
+    /// #     worksheet.write_number(i, 2, (i*10 + 4).into(), None)?;
+    /// # }
+    /// # Ok(())
+    /// # }
+    /// ```
+    /// ![Result Image](https://github.com/informationsea/xlsxwriter-rs/raw/master/images/test-chart_series-set_fill-1.png)
+    pub fn set_fill(&mut self, fill: &ChartFill) {
+        unsafe {
+            libxlsxwriter_sys::chart_series_set_fill(self.chart_series, &mut fill.value());
+        }
+    }
+
+    /// Invert the fill color for negative values. Usually only applicable to column and bar charts.
+    /// ```rust
+    /// # use xlsxwriter::*;
+    /// # fn main() { let _ = run(); }
+    /// # fn run() -> Result<(), XlsxError> {
+    /// # let workbook = Workbook::new("test-chart_series-set_invert_if_negative-1.xlsx");
+    /// # let mut worksheet = workbook.add_worksheet(None)?;
+    /// # write_worksheet(&mut worksheet)?; // write worksheet contents
+    /// # let mut chart = workbook.add_chart(ChartType::Column);
+    /// # let mut series1 = chart.add_series(None, Some("=Sheet1!$A$2:$A$6"));
+    /// # let mut series2 = chart.add_series(None, Some("=Sheet1!$B$2:$B$6"));
+    /// # let mut series3 = chart.add_series(None, Some("=Sheet1!$C$2:$C$6"));
+    /// # let mut chart_fill_1 = ChartFill::new();
+    /// # chart_fill_1.color = FormatColor::Red;
+    /// # let mut chart_fill_2 = ChartFill::new();
+    /// # chart_fill_2.color = FormatColor::Yellow;
+    /// # let mut chart_fill_3 = ChartFill::new();
+    /// # chart_fill_3.color = FormatColor::Green;
+    /// # series1.set_fill(&chart_fill_1);
+    /// series1.set_invert_if_negative();
+    /// # series2.set_fill(&chart_fill_2);
+    /// # series2.set_invert_if_negative();
+    /// # series3.set_fill(&chart_fill_3);
+    /// # series3.set_invert_if_negative();
+    /// # worksheet.insert_chart(1, 3, &chart)?;
+    /// # workbook.close()
+    /// # }
+    /// # fn write_worksheet(worksheet: &mut Worksheet) -> Result<(), XlsxError> {
+    /// # worksheet.write_string(0, 0, "Set 1", None)?;
+    /// # worksheet.write_string(0, 1, "Set 2", None)?;
+    /// # worksheet.write_string(0, 2, "Set 3", None)?;
+    /// # for i in 0..6 {
+    /// #     let j: f64 = i.into();
+    /// #     worksheet.write_number(i, 0, (j*10.) - 20., None)?;
+    /// #     worksheet.write_number(i, 1, (j*10. + 2.) - 20., None)?;
+    /// #     worksheet.write_number(i, 2, (j*10. + 4.) - 20., None)?;
+    /// # }
+    /// # Ok(())
+    /// # }
+    /// ```
+    pub fn set_invert_if_negative(&mut self) {
+        unsafe {
+            libxlsxwriter_sys::chart_series_set_invert_if_negative(self.chart_series);
         }
     }
 }
